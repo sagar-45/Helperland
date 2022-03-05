0845a0d7aa3fa43205b996cda51e629a54b1050c
sagar-45 <63908993+sagar-45@users.noreply.github.com>
2022-03-05 08:36:38 -0800

    Update header1.php

--- MVC/HelperLand/views/header1.php
+++ MVC/HelperLand/views/header1.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <title>Helperland</title>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/header1.css" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/login_forgot_card.css" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/<?php echo "$function" ?>.css" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/footer2.css" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/media.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
-  <script>
-    function login_set()
-    {
-      sessionStorage.afterPages=2;
-    }
-  </script>
-</head>
-
-<body id="<?php echo "$function" ?>">
 <?php $base_url="http://localhost/HelperLand/" ;?>
   <!-- home section start-->
   <section id="header">
@@ -53,7 +29,7 @@
               <a class="nav-link" href="<?= $base_url.'?controller=Home&&function=contact'?>">Contact us</a>
             </li>
             <li class="nav-item login dark" data-bs-toggle="modal" data-bs-target="#login">
-              <a class="nav-link" href="<?= $base_url.'?controller=Home&&function=index'?>" onclick="login_set()">Login</a>
+              <a class="nav-link" onclick="login_set()">Login</a>
             </li>
             <li class="nav-item helper dark">
               <a class="nav-link" href="<?= $base_url.'?controller=Home&&function=service'?>">Become a Helper</a>
