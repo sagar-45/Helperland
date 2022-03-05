fd9e62d1a64dff97c467d9e2b74dacbce6577bee
sagar-45 <63908993+sagar-45@users.noreply.github.com>
2022-03-05 08:34:55 -0800

    Update c_service_history.php

--- MVC/HelperLand/views/c_service_history.php
+++ MVC/HelperLand/views/c_service_history.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Helperland</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/header3.css" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/pagination.css" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/footer2.css" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/serviceH.css" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/media.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+</head>
+<?php
+$status1=3;
+$status2=4;
+?>
+<body id="serviceH" onload="loadnewServiceRequest(<?php echo $_SESSION['userId'] ?>,<?php echo $status1 ?>,<?php echo $status2 ?>)">
+<div class="preloader"></div>
 <?php
     require_once('header3.php');
 ?>
@@ -6,123 +30,71 @@
     <p>Welcome,<b><?php echo $_SESSION['UserName'] ?></b></p>
   </div>
   <div class="hr"></div>
-<?php
-    require_once('c_list_page.php');
-  ?>
+  <div class="modal fade" id="details_request" tabindex="-1" aria-hidden="true">
+  </div>
+  <div class="modal fade" id="rate_sp" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+    </div>
+  </div>
+  <div class="mid-dash">
+    <section id="list">
+      <div class="list-group">
+        <a class="list-group-item" href="<?= $base_url.'?controller=Home&&function=customer_dashboard' ?>">Dashboard</a>
+        <a class="list-group-item active">Service History</a>
+        <a class="list-group-item">Service Schedule</a>
+        <a class="list-group-item">Favourite Pros</a>
+        <a class="list-group-item">Invoices</a>
+        <a class="list-group-item">Notification</a>
+      </div>
+    </section>
   <div class="table_page">
       <div class="service_head">
         <span class="service_txt">Service History</span>
-        <button class="btn export">Export</button>
       </div>
       <section id="service_history">
         <table class="table">
           <thead>
             <tr>
-              <th scope="col">
-                Service Details<img src="./assets/images/cross.png" class="img-fluid" />
+            <th scope="col">
+                Service Id
               </th>
               <th scope="col">
-                Service Provider<img src="./assets/images/cross.png" class="img-fluid" />
+                Service Details
               </th>
               <th scope="col">
-                Payment<img src="./assets/images/cross.png" class="img-fluid" />
+                Service Provider
               </th>
               <th scope="col">
-                Status<img src="./assets/images/cross.png" class="img-fluid" />
+                Payment
               </th>
               <th scope="col">
-                Rate SP<img src="./assets/images/cross.png" class="img-fluid" />
+                Status
+              </th>
+              <th scope="col">
+                Rate SP
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <div class="date">
-                  <img src="./assets/images/calendar_black.png" class="img-fluid" />
-                  <span>31/03/2018 </span>
-                </div>
-                <span>12:00 - 18:00</span>
-              </td>
-              <td>
-                <div class="rating">
-                  <img src="./assets/images/cap.png" class="img-fluid cap" />
-                  <div class="star">
-                    <p>Lyum Watson</p>
-                    <img src="./assets/images/star1.png" class="img-fluid" />
-                    <img src="./assets/images/star1.png" class="img-fluid" />
-                    <img src="./assets/images/star1.png" class="img-fluid" />
-                    <img src="./assets/images/star1.png" class="img-fluid" />
-                    <img src="./assets/images/star2.png" class="img-fluid" />
-                    <span>4</span>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <div class="rupee">
-                  <span class="uro">€</span><strong class="rs">63</strong>
-                </div>
-              </td>
-              <td>
-                <div class="status-c">
-                  <p class="text">Completed</p>
-                </div>
-              </td>
-              <td>
-                <button class="btn">Rate SP</button>
-              </td>
-            </tr>
           </tbody>
         </table>
       </section>
-    <?php
-    require_once('c_pagination.php');
-    ?>
-    <div class="mobile-view mobile-card">
-    <div class="card_name">
-      <b>Services History</b>
     </div>
-<div class="card">
-      <div class="card-body">
-        <div class="date">
-          <img src="./assets/images/calendar_black.png" class="img-fluid" />
-          <span>01/01/2018 </span>
-        </div>
-        <span>12:00 - 18:00</span>
-        <hr />
-        <div class="rating">
-          <img src="./assets/images/cap.png" class="img-fluid cap" />
-          <div class="star">
-            <p>Lyum Watson</p>
-            <img src="./assets/images/star1.png" class="img-fluid" />
-            <img src="./assets/images/star1.png" class="img-fluid" />
-            <img src="./assets/images/star1.png" class="img-fluid" />
-            <img src="./assets/images/star1.png" class="img-fluid" />
-            <img src="./assets/images/star2.png" class="img-fluid" />
-            <span>4</span>
-          </div>
-        </div>
-        <hr />
-        <div class="rupee">
-          <span class="uro">€</span><strong class="rs">63</strong>
-        </div>
-        <hr />
-        <div class="status-d">
-          <p class="text">Cancelled</p>
-        </div>
-        <hr />
-        <button class="btn">Rate SP</button>
-      </div>
-    </div>
+  </div>
     <?php
-    require_once('c_mobile_pagination.php');
     require_once('footer2.php')
-?>
+    ?>
   <script src="./assets/js/helperland.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.js"></script>
+  <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
 </body>
 
 </html>
