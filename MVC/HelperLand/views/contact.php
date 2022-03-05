443ad44b7671b1ea3183078ff9667388928a5e4e
sagar-45 <63908993+sagar-45@users.noreply.github.com>
2022-03-05 08:35:31 -0800

    Update contact.php

--- MVC/HelperLand/views/contact.php
+++ MVC/HelperLand/views/contact.php
@@ -1,3 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Helperland</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <?php
+    if(isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId']==1)
+    {
+        echo '<link rel="stylesheet" type="text/css" href="./assets/css/header3.css" />'; 
+    }
+    elseif(isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId']==2)
+    {
+        echo '<link rel="stylesheet" type="text/css" href="./assets/css/header4.css" />';
+    }
+    else
+    {
+        echo '<link rel="stylesheet" type="text/css" href="./assets/css/header2.css" />';
+    }
+  ?>
+  <link rel="stylesheet" type="text/css" href="./assets/css/contact.css" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/footer2.css" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/media.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+  <script>
+    function login_set()
+    {
+      sessionStorage.afterPages=2;
+    }
+  </script>
+</head>
+
+<body id="contact">
+
 <?php
   if(isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId']==1)
   {
@@ -111,6 +148,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </body>
 
 </html>
