1d323e2fcdb43a8c98a56c6016dfa61bdadb8182
sagar-45 <63908993+sagar-45@users.noreply.github.com>
2022-03-05 08:37:00 -0800

    Update header2.php

--- MVC/HelperLand/views/header2.php
+++ MVC/HelperLand/views/header2.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <title>Helperland</title>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/footer2.css" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/login_forgot_card.css" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/header2.css" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/<?php echo "$function"?>.css" />
-  <link rel="stylesheet" type="text/css" href="./assets/css/media.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
-  <script>
-    function login_set()
-    {
-      sessionStorage.afterPages=2;
-    }
-  </script>
-</head>
-
-<body id="<?php echo "$function" ?>">
 <?php $base_url="http://localhost/HelperLand/" ;?>
   <!-- header section start -->
   <section id="header">
