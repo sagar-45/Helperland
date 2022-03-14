7550287c293fb777c455b53a590d74fed2651c53
sagar-45 <63908993+sagar-45@users.noreply.github.com>
2022-03-14 02:11:00 -0700

    Update Email_exist_modal.php

--- MVC/HelperLand/views/Email_exist_modal.php
+++ MVC/HelperLand/views/Email_exist_modal.php
@@ -1,10 +1,10 @@
 <button type="button" class="btn email_exist" data-bs-toggle="modal" data-bs-target="#exist"></button>
-    <div class="modal fade" id="exist"  data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <span class="false"><img src="./assets/images/cross_error.png" /></span>
-                <p>Email already registered</p>
-                <button type="button" class="btn exist_btn" data-bs-dismiss="modal">OK</button>
-            </div>
+<div class="modal fade" id="exist" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <span class="false"><img src="./assets/images/cross_error.png" /></span>
+            <p>Email already registered</p>
+            <button type="button" class="btn exist_btn" data-bs-dismiss="modal">OK</button>
         </div>
     </div>
+</div>
