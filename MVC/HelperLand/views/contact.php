8f42b764f5e73742fa241ccf0887642830c5ae55
sagar-45 <63908993+sagar-45@users.noreply.github.com>
2022-03-14 02:14:47 -0700

    Update contact.php

--- MVC/HelperLand/views/contact.php
+++ MVC/HelperLand/views/contact.php
@@ -6,53 +6,41 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <?php
-    if(isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId']==1)
-    {
-        echo '<link rel="stylesheet" type="text/css" href="./assets/css/header3.css" />'; 
-    }
-    elseif(isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId']==2)
-    {
-        echo '<link rel="stylesheet" type="text/css" href="./assets/css/header4.css" />';
-    }
-    else
-    {
-        echo '<link rel="stylesheet" type="text/css" href="./assets/css/header2.css" />';
-    }
+  if (isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId'] == 1) {
+    echo '<link rel="stylesheet" type="text/css" href="./assets/css/header3.css" />';
+  } elseif (isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId'] == 2) {
+    echo '<link rel="stylesheet" type="text/css" href="./assets/css/header4.css" />';
+  } else {
+    echo '<link rel="stylesheet" type="text/css" href="./assets/css/header2.css" />';
+  }
   ?>
   <link rel="stylesheet" type="text/css" href="./assets/css/contact.css" />
   <link rel="stylesheet" type="text/css" href="./assets/css/footer2.css" />
   <link rel="stylesheet" type="text/css" href="./assets/css/media.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
   <script>
-    function login_set()
-    {
-      sessionStorage.afterPages=2;
+    function login_set() {
+      sessionStorage.afterPages = 2;
     }
   </script>
 </head>
 
 <body id="contact">
 
-<?php
-  if(isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId']==1)
-  {
-      require_once('header3.php');
-  }
-  elseif(isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId']==2)
-  {
+  <?php
+  if (isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId'] == 1) {
+    require_once('header3.php');
+  } elseif (isset($_SESSION['UserTypeId']) && $_SESSION['UserTypeId'] == 2) {
     require_once('header4.php');
-  }
-  else
-  {
+  } else {
     include('header2.php');
     require_once('navbar_mobile_view.php');
   }
-?>
-</section>
-<!-- contact image section start -->
-<section id="contact-img">
+  ?>
+  </section>
+  <!-- contact image section start -->
+  <section id="contact-img">
     <img src="./assets/images/contact-img.png" class="img-fluid" alt="" />
   </section>
   <!-- contact image section end -->
@@ -93,7 +81,7 @@
   <section id="touch">
     <div class="touch-head">Get in touch with us</div>
     <div class="contact-form">
-      <form action="<?= $base_url.'?controller=Home&&function=getTouchwithUs'?>" method="POST" enctype="multipart/form-data">
+      <form action="<?= $base_url . '?controller=Home&&function=getTouchwithUs' ?>" method="POST" enctype="multipart/form-data">
         <div class="personal-detail">
           <input type="text" name="fname" placeholder="First name" />
           <input type="text" name="lname" placeholder="Last name" />
@@ -119,15 +107,15 @@
           <textarea name='message' placeholder="Message"></textarea>
         </div>
         <div class="attech">
-          Attachment<br/>
+          Attachment<br />
           <input name="i1" type="file" id="file">
-          <label  for="file">Upload</label>
+          <label for="file">Upload</label>
         </div>
         <div class="privacy">
-          <div class="checkinput"><input type="checkbox" id="check"/></div>
+          <div class="checkinput"><input type="checkbox" id="check" /></div>
           <div class="policies"><label for="check">Our current ones apply <span class="colortext">privacy policy.</span>I hereby agree that my data entered into the contact
-          form will be stored electronically and processed and used for the purpose of establishing contact.The
-          consent can be withdrawn at any time pursuant to Art.7(3) GDPR by informal notification (eg by e-mail).</label></div>
+              form will be stored electronically and processed and used for the purpose of establishing contact.The
+              consent can be withdrawn at any time pursuant to Art.7(3) GDPR by informal notification (eg by e-mail).</label></div>
         </div>
         <input type="submit" class="btn submit" />
       </form>
@@ -141,13 +129,11 @@
   </section>
   <!-- map image section end -->
   <?php
-    include('news_letter.php');
-    include('footer2.php');
+  include('news_letter.php');
+  include('footer2.php');
   ?>
   <script src="./assets/js/helperland.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </body>
 
